3db18aeb37f97113dd7e56c81960c59d60ec9ab8
jr-cho <joshmg426@gmail.com>
2025-11-03 16:21:54 -0500

    Initial Setup

---
+++ client/client.py
@@ -0,0 +1,21 @@
+# client.py
+
+class client:
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
+
+    def update(self, name, ip):
+        pass
+
+    def remove(self):
+        pass
+
+    def client_init(client):
+        pass
+
+    def client_connect(client, server):
+        pass
+
+    def something():
+        pass
---
+++ server/server.py
@@ -0,0 +1,18 @@
+# server.py
+
+class server:
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
+
+    def update(self, name, ip):
+        pass
+
+    def remove(self):
+        pass
+
+    def server_init(self):
+        pass
+
+    def server_allow_connect(self):
+        pass
